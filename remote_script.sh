23b319d3fc9a06d5fe4cde25f45747af322bc001
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-03-14 00:28:21 -0400

    finished. functions were breaking stuff. works perfect now 🤘 .

--- remote_script.sh
+++ remote_script.sh
@@ -1,16 +1,2 @@
-default_first_alphebetical() {
-  url=$(git remote -v | sed -n 1p | grep fetch | cut -c 8- | sed '/\n/!G;s/\(.\)\(.*\n\)/&\2\1/;//D;s/.//' | cut -c 13- | sed '/\n/!G;s/\(.\)\(.*\n\)/&\2\1/;//D;s/.//')
-  open $url
-}
-pass_in_remote() {
   url=$(git remote -v | grep "$1" | grep fetch | cut -c 8- | sed '/\n/!G;s/\(.\)\(.*\n\)/&\2\1/;//D;s/.//' | cut -c 13- | sed '/\n/!G;s/\(.\)\(.*\n\)/&\2\1/;//D;s/.//')
   open $url
-}
-default_to_origin() {
-  url=$(git remote -v | grep origin | grep fetch | cut -c 8- | sed '/\n/!G;s/\(.\)\(.*\n\)/&\2\1/;//D;s/.//' | cut -c 13- | sed '/\n/!G;s/\(.\)\(.*\n\)/&\2\1/;//D;s/.//')
-  open $url
-}
-
-default_first_alphebetical
-pass_in_remote
-default_to_origin
