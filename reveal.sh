eabec05baae24c7f49c9cabc813e748d245786c2
Jacob Menke <jamenk@email.wm.edu>
2018-12-29 15:36:13 -0500

    reveal function in reveal.sh

---
+++ reveal.sh
@@ -0,0 +1,22 @@
+reveal() {
+    OS="$(uname -s)"
+    case "$OS" in
+        Linux*)     open_cmd=xdg-open;;
+        Darwin*)    open_cmd=open;;
+        CYGWIN*)    open_cmd=cygstart;;
+        MINGW*)     open_cmd=start;;
+        *)          open_cmd="UNKNOWN:$OS"
+                    echo "Your OS: $OS is unsupported..." >&2 && return 2;;
+    esac
+
+    [[ ! -d .git ]] && echo "Not git dir" >&2 && return 1;
+
+    git remote -v | grep "$1" | grep 'heroku' | grep fetch | grep -o -E ':.*' | \
+    cut -c 19- | awk '{print $1}' | sed 's@.git$@@' | \
+    xargs -I {} $open_cmd https://dashboard.heroku.com/apps/{} https://{}.herokuapp.com
+  {
+    git remote -v | grep "$1" | grep '@'  | grep -o -E '@.*' | cut -c 2-;
+    git remote -v | grep "$1" | grep '//' | grep -o -E ':.*' | cut -c 4- | grep -v 'heroku';
+  } | grep fetch | sed -e $'s@:/\\@@g' |  awk '{print $1}' | sed 's@.git$@@' | xargs -I {} $open_cmd https://www.{}
+}
+
