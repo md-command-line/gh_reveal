d305669cfc980989651b21e30fc4c637cbf19927
Jacob Menke <jamenk@email.wm.edu>
2018-12-30 18:08:26 -0500

    make sure grep is not an alias

--- reveal.sh
+++ reveal.sh
@@ -1,4 +1,5 @@
 reveal() {
+    set -x
     OS="$(uname -s)"
     case "$OS" in
         Linux*)     open_cmd=xdg-open;;
@@ -6,17 +7,19 @@ reveal() {
         CYGWIN*)    open_cmd=cygstart;;
         MINGW*)     open_cmd=start;;
         *)          open_cmd="UNKNOWN:$OS"
-                    echo "Your OS: $OS is unsupported..." >&2 && return 2;;
+        echo "Your OS: $OS is unsupported..." >&2 && return 2;;
     esac
 
-    [[ ! -d .git ]] && echo "Not git dir" >&2 && return 1;
+    [[ ! -d .git ]] && echo "Not a git directory" >&2 && return 1
 
-    git remote -v | grep "$1" | grep 'heroku' | grep fetch | grep -o -E ':.*' | \
+    git remote -v | \grep "$1" | \grep 'heroku' | \grep fetch | \grep -o -E ':.*' | \
     cut -c 19- | awk '{print $1}' | sed 's@.git$@@' | \
-    xargs -I {} $open_cmd https://dashboard.heroku.com/apps/{} https://{}.herokuapp.com
-  {
-    git remote -v | grep "$1" | grep '@'  | grep -o -E '@.*' | cut -c 2-;
-    git remote -v | grep "$1" | grep '//' | grep -o -E ':.*' | cut -c 4- | grep -v 'heroku';
-  } | grep fetch | sed -e $'s@:/\\@@g' |  awk '{print $1}' | sed 's@.git$@@' | xargs -I {} $open_cmd https://www.{}
-}
+    xargs -I {} "$open_cmd" https://dashboard.heroku.com/apps/{} https://{}.herokuapp.com
 
+  {
+    git remote -v | \grep "$1" | \grep '@'  | \grep -o -E '@.*' | cut -c 2-;
+    git remote -v | \grep "$1" | \grep '//' | \grep -o -E ':.*' | cut -c 4- | \grep -v 'heroku';
+  } | \grep fetch | sed 's@:/\\@@g' | awk '{print $1}' | sed 's@.git$@@' | xargs -I {} "$open_cmd" https://www.{}
+
+set +x
+}
