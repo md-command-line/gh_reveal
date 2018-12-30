148e04d69a01c948f4b72156eee9e796adb87813
Jacob Menke <jamenk@email.wm.edu>
2018-12-30 18:18:41 -0500

    remove extra semicolons

--- reveal.sh
+++ reveal.sh
@@ -5,8 +5,7 @@ reveal() {
         Darwin*)    open_cmd=open;;
         CYGWIN*)    open_cmd=cygstart;;
         MINGW*)     open_cmd=start;;
-        *)          open_cmd="UNKNOWN:$OS"
-        echo "Your OS: $OS is unsupported..." >&2 && return 2;;
+        *)          echo "Your OS: $OS is unsupported..." >&2 && return 2;;
     esac
 
     [[ ! -d .git ]] && echo "Not a git directory" >&2 && return 1
@@ -16,8 +15,8 @@ reveal() {
     xargs -I {} "$open_cmd" https://dashboard.heroku.com/apps/{} https://{}.herokuapp.com
 
   {
-    git remote -v | \grep "$1" | \grep '@'  | \grep -o -E '@.*' | cut -c 2-;
-    git remote -v | \grep "$1" | \grep '//' | \grep -o -E ':.*' | cut -c 4- | \grep -v 'heroku';
+    git remote -v | \grep "$1" | \grep '@'  | \grep -o -E '@.*' | cut -c 2-
+    git remote -v | \grep "$1" | \grep '//' | \grep -o -E ':.*' | cut -c 4- | \grep -v 'heroku'
   } | \grep fetch | sed 's@:/\\@@g' | awk '{print $1}' | sed 's@.git$@@' | xargs -I {} "$open_cmd" https://www.{}
 
 }
