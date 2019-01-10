340ccd23f80925bd53aadfe2f07299025506a3ec
michaeldimmitt <michaelgdimmitt@gmail.com>
2019-01-10 13:10:05 -0500

    Any number of arguments to specify remotes. mac verified

--- reveal.sh
+++ reveal.sh
@@ -15,12 +15,13 @@ reveal() {
     $OS https://github.com/$name?tab=repositories
   fi
 
-  command git remote -v | command grep "$1" | command grep 'heroku' | command grep fetch | command grep -o -E ':.*' | \
+  argValues="$*";
+  command git remote -v | command grep "$(echo ${argValues// /\\|})" | command grep 'heroku' | command grep fetch | command grep -o -E ':.*' | \
   cut -c 19- | command awk '{print $1}' | command sed 's@.git$@@' | \
   command xargs -I {} "$open_cmd" https://dashboard.heroku.com/apps/{} https://{}.herokuapp.com
 
   {
-    command git remote -v | command grep "$1" | command grep '@'  | command grep -o -E '@.*' | cut -c 2-
-    command git remote -v | command grep "$1" | command grep '//' | command grep -o -E ':.*' | cut -c 4- | command grep -v 'heroku'
+    command git remote -v | command grep "$(echo ${argValues// /\\|})" | command grep '@'  | command grep -o -E '@.*' | cut -c 2-
+    command git remote -v | command grep "$(echo ${argValues// /\\|})" | command grep '//' | command grep -o -E ':.*' | cut -c 4- | command grep -v 'heroku'
   } | command grep fetch | command sed 's@:/\\@@g' | command awk '{print $1}' | sed 's@.git$@@' | command xargs -I {} "$open_cmd" https://www.{}
 }
