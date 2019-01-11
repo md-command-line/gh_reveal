684bc526a7b4ffab3e292460d2924773b2984de7
michaeldimmitt <michaelgdimmitt@gmail.com>
2019-01-11 13:03:24 -0500

    git clone install and uninstall instructions

---
+++ install.sh
@@ -0,0 +1,3 @@
+curl -fsSL -o /usr/local/bin/reveal https://raw.githubusercontent.com/MenkeTechnologies/gh_reveal/master/reveal.sh &&
+echo 'reveal "$@"' >> /usr/local/bin/reveal &&
+chmod +x /usr/local/bin/reveal
