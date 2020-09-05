c7806ff1225ca32401947bc9b0f58e1ab2a5a148
MenkeTechnologies <linux.dev25@gmail.com>
2020-09-05 18:17:14 -0400

    autoload reveal

--- gh_reveal.plugin.zsh
+++ gh_reveal.plugin.zsh
@@ -1,2 +1,5 @@
-source "${0:h}/reveal"
+0="${${0:#$ZSH_ARGZERO}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
 
+fpath+="${0:h}/bin"
+autoload -Uz reveal
--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 source ./credential.sh
 install(){
-  curl -fsSL -o /usr/local/bin/reveal https://raw.githubusercontent.com/michaeldimmitt/gh_reveal/master/reveal.sh &&
-  echo 'reveal "$@"' >> /usr/local/bin/reveal &&
+  curl -fsSL -o /usr/local/bin/reveal https://raw.githubusercontent.com/michaeldimmitt/gh_reveal/master/bin/reveal &&
   chmod +x /usr/local/bin/reveal;
 }
 check_bin_folder_and_authenticate install
