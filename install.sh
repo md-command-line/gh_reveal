036a0940ae868763389e1007507879aa6d4edd9d
MenkeTechnologies <linux.dev25@gmail.com>
2020-09-08 21:54:32 -0400

    add unicode chars for install messaging

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 source ./credential.sh
 install(){
     curl -fsSL -o /usr/local/bin/reveal https://raw.githubusercontent.com/md-command-line/gh_reveal/master/bin/reveal &&
-    chmod +x /usr/local/bin/reveal &&
-    echo "Successfully installed /usr/local/bin/reveal!" ||
-    echo "usr/local/bin/reveal uninstall failed" >&2
+    chmod +x /usr/local/bin/reveal
 }
-check_bin_folder_and_authenticate install
+check_bin_folder_and_authenticate install &&
+    printf "\u2714 Successfully installed /usr/local/bin/reveal!\n" ||
+    printf "\u2718 usr/local/bin/reveal uninstall failed.\n" >&2
--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 source ./credential.sh
 uninstall(){
-  rm -rf /usr/local/bin/reveal && echo "/usr/local/bin/reveal successfully uninstalled." || echo "usr/local/bin/reveal uninstall failed" >&2
+  rm -rf /usr/local/bin/reveal 
 }
 
-check_bin_folder_and_authenticate uninstall
+check_bin_folder_and_authenticate uninstall &&
+    printf "\u2714 /usr/local/bin/reveal successfully uninstalled.\n" ||
+    printf "\u2718 /usr/local/bin/reveal uninstall failed.\n" >&2
